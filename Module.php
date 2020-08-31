6cb2425a3c983a7a9fdedc31c7d0820d24cf9722
Daniel Berthereau <Daniel.git@Berthereau.net>
2020-08-31 00:00:00 +0000

    Sorted resource template by label in resource form.

--- Module.php
+++ Module.php
@@ -9,8 +9,52 @@ if (!class_exists(\Generic\AbstractModule::class)) {
 }
 
 use Generic\AbstractModule;
+use Laminas\EventManager\Event;
+use Laminas\EventManager\SharedEventManagerInterface;
 
 class Module extends AbstractModule
 {
     const NAMESPACE = __NAMESPACE__;
+
+    public function attachListeners(SharedEventManagerInterface $sharedEventManager)
+    {
+        $sharedEventManager->attach(
+            \Omeka\Form\ResourceForm::class,
+            'form.add_elements',
+            [$this, 'fixResourceForm']
+        );
+    }
+
+    public function fixResourceForm(Event $event)
+    {
+        /** @var \Omeka\Form\ResourceForm $form */
+        $form = $event->getTarget();
+        // Due to the closure (?), the element should be removed first.
+        $form
+            ->remove('o:resource_template[o:id]')
+            ->add([
+                'name' => 'o:resource_template[o:id]',
+                'type' => \Omeka\Form\Element\ResourceSelect::class,
+                'attributes' => [
+                    'id' => 'resource-template-select',
+                    'class' => 'chosen-select',
+                    'data-placeholder' => 'Select a template', // @translate
+                    'data-api-base-url' => $form->getUrlHelper()->__invoke('api/default', ['resource' => 'resource_templates']),
+                ],
+                'options' => [
+                    'label' => 'Resource template', // @translate
+                    'info' => 'A pre-defined template for resource creation.', // @translate
+                    'empty_option' => '',
+                    'resource_value_options' => [
+                        'resource' => 'resource_templates',
+                        'query' => [
+                            'sort_by' => 'label',
+                        ],
+                        'option_text_callback' => function ($resourceTemplate) {
+                            return $resourceTemplate->label();
+                        },
+                    ],
+                ],
+            ]);
+    }
 }
