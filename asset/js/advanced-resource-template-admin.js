68e7386f9299c2fa29e1928ac793339ac001ed8e
Daniel Berthereau <Daniel.git@Berthereau.net>
2020-08-31 00:00:00 +0000

    Added a value only for new resource and if there is no value yet.

--- asset/js/advanced-resource-template-admin.js
+++ asset/js/advanced-resource-template-admin.js
@@ -13,8 +13,16 @@ $(document).on('o:prepare-value', function(e, dataType, value, valueObj, term) {
         return;
     }
 
-    if (data.default_value && data.default_value.length && !valueObj) {
-        value.find('textarea.input-value').val(data.default_value);
+    // Manage default value.
+    if (data.default_value && data.default_value.length
+        // The value from the object is already managed.
+        && !valueObj
+        // Don't add a value if this is an edition.
+        && $('body').hasClass('add')
+        // Don't add a value if there is already a value.
+        && field.find('.input-value').length === 0
+    ) {
+        value.find('.input-value').val(data.default_value);
     }
 });
 
