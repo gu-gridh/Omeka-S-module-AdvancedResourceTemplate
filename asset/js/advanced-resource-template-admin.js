32a6d2c04c97bf6cc292a230a07d155417afc4d5
Daniel Berthereau <Daniel.git@Berthereau.net>
2020-09-07 00:00:00 +0000

    Cleaned creation of empty and new values in js.

--- asset/js/advanced-resource-template-admin.js
+++ asset/js/advanced-resource-template-admin.js
@@ -162,63 +162,41 @@ $(document).ready(function() {
     }
 
     /**
-     * Manage default value.
+     * Fill a new value, that can be empty.
      *
-     * The default value can be a json value, a simple string, an integer, or a uri + a string.
-     * @see resource-form.js, same hook (but this one comes after).
+     * @see makeNewValue() in resource-form.js: the same, except that the empty
+     * value is already created in previous hook and may be filled.
      */
-    function fillDefaultValue(dataType, value, valueObj, field) {
-        var settings = field.data('settings');
-        if (!settings
-            || !settings.default_value || !settings.default_value.trim().length
-            // The value from the object is already managed.
-            || valueObj
-            // Don't add a value if this is an edition.
-            || !$('body').hasClass('add')
-            // Don't add a value if there is already a value.
-            || field.find('.input-value').length > 0
-            || field.find('input.value').length > 0
-            || field.find('input.value.to-require').length > 0
-            // Custom vocab.
-            || field.find('select.terms option').length > 0
-            // Numeric data types.
-            || field.find('input.numeric-integer-value').length > 0
-            // Value suggest.
-            || field.find('input.valuesuggest-input').length > 0
-        ) {
-            return;
-        }
-
-        var defaultValue = settings.default_value.trim();
-        valueObj = jsonDecodeObject(defaultValue);
+    function fillValue(value, term, dataType, valueObj) {
+        // If defaultValue is undefined, it means that valueObj is filled.
+        var defaultValue = valueObj.default;
+        var isSpecific = typeof defaultValue !== 'undefined';
         // Manage specific data type "resource".
         if (dataType.startsWith('resource')) {
-            if (/^\d+$/.test(defaultValue)) {
-                if (!valueObj) {
-                    valueObj = {
-                        display_title: Omeka.jsTranslate('Resource') + ' #' + defaultValue,
-                        value_resource_id: defaultValue,
-                        value_resource_name: 'resource',
-                        url: '#',
-                    };
-                }
-                value.find('input.value[data-value-key="value_resource_id"]').val(valueObj.value_resource_id);
-                // TODO Get the title from the api (when authentication will be opened).
-                value.find('span.default').hide();
-                var resource = value.find('.selected-resource');
+            if (isSpecific && /^\d+$/.test(defaultValue)) {
+                valueObj = {
+                    display_title: Omeka.jsTranslate('Resource') + ' #' + defaultValue,
+                    value_resource_id: defaultValue,
+                    value_resource_name: 'resource',
+                    url: '#',
+                };
+            }
+            value.find('input.value[data-value-key="value_resource_id"]').val(valueObj['value_resource_id']);
+            // TODO Get the title from the api (when authentication will be opened).
+            value.find('span.default').hide();
+            var resource = value.find('.selected-resource');
+            resource.find('.o-title')
+                .removeClass() // remove all classes
+                .addClass('o-title ' + valueObj['value_resource_name'])
+                .html($('<a>', {href: valueObj['url'], text: valueObj['display_title']}));
+            if (typeof valueObj['thumbnail_url'] !== 'undefined') {
                 resource.find('.o-title')
-                    .removeClass() // remove all classes
-                    .addClass('o-title ' + valueObj['value_resource_name'])
-                    .html($('<a>', {href: valueObj['url'], text: valueObj['display_title']}));
-                if (typeof valueObj['thumbnail_url'] !== 'undefined') {
-                    resource.find('.o-title')
-                        .prepend($('<img>', {src: valueObj['thumbnail_url']}));
-                }
+                    .prepend($('<img>', {src: valueObj['thumbnail_url']}));
             }
         }
 
         // Manage most common default values for other data types.
-        if (!valueObj) {
+        if (isSpecific) {
             if (dataType === 'uri' || dataType.startsWith('valuesuggest')) {
                 if (defaultValue.match(/^(\S+)\s(.*)/)) {
                     valueObj = defaultValue.match(/^(\S+)\s(.*)/).slice(1);
@@ -418,7 +396,29 @@ $(document).ready(function() {
 
         fillDefaultLanguage(value, valueObj, field);
 
-        fillDefaultValue(dataType, value, valueObj, field);
+        // Fill a new value with the setting specified in the template.
+        // This is not the same than the default value in resource-form.js, that is an empty value.
+        if (!settings.default_value || !settings.default_value.trim().length
+            // The value from the object is already managed.
+            || valueObj
+            // Don't add a value if this is an edition.
+            || !$('body').hasClass('add')
+            // Don't add a value if there is already a value.
+            || field.find('.input-value').length > 0
+            || field.find('input.value').length > 0
+            || field.find('input.value.to-require').length > 0
+            // Custom vocab.
+            || field.find('select.terms option').length > 0
+            // Numeric data types.
+            || field.find('input.numeric-integer-value').length > 0
+            // Value suggest.
+            || field.find('input.valuesuggest-input').length > 0
+        ) {
+            return;
+        } else {
+            var defaultValue = jsonDecodeObject(settings.default_value);
+            fillValue(value, term, dataType, defaultValue === null ? {default: settings.default_value.trim()} : defaultValue);
+        }
     });
 
     var modal;
