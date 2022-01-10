7cbcdf0e6a4eb457c7f485abe901af25e79e1c8a
Daniel Berthereau <Daniel.git@Berthereau.net>
2022-01-10 00:00:00 +0000

    Fixed strict type issue.

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -13,37 +13,36 @@ namespace AdvancedResourceTemplate;
  * @var \Omeka\Api\Manager $api
  */
 $services = $serviceLocator;
-$settings = $services->get('Omeka\Settings');
-$config = require dirname(__DIR__, 2) . '/config/module.config.php';
+// $settings = $services->get('Omeka\Settings');
+// $config = require dirname(__DIR__, 2) . '/config/module.config.php';
 $connection = $services->get('Omeka\Connection');
-$entityManager = $services->get('Omeka\EntityManager');
-$plugins = $services->get('ControllerPluginManager');
-$api = $plugins->get('api');
-$space = strtolower(__NAMESPACE__);
+// $entityManager = $services->get('Omeka\EntityManager');
+// $plugins = $services->get('ControllerPluginManager');
+// $api = $plugins->get('api');
 
-if (version_compare($oldVersion, '3.3.3.3', '<')) {
+if (version_compare((string) $oldVersion, '3.3.3.3', '<')) {
     $this->execSqlFromFile($this->modulePath() . '/data/install/schema.sql');
 }
 
-if (version_compare($oldVersion, '3.3.4', '<')) {
+if (version_compare((string) $oldVersion, '3.3.4', '<')) {
     $sql = <<<'SQL'
 ALTER TABLE `resource_template_property_data`
 DROP INDEX UNIQ_B133BBAA2A6B767B,
 ADD INDEX IDX_B133BBAA2A6B767B (`resource_template_property_id`);
 SQL;
-    $connection->exec($sql);
+    $connection->executeStatement($sql);
 }
 
-if (version_compare($oldVersion, '3.3.4.3', '<')) {
+if (version_compare((string) $oldVersion, '3.3.4.3', '<')) {
     // @link https://www.doctrine-project.org/projects/doctrine-dbal/en/2.6/reference/types.html#array-types
     $sql = <<<'SQL'
 ALTER TABLE `resource_template_data`
 CHANGE `data` `data` LONGTEXT NOT NULL COMMENT '(DC2Type:json)';
 SQL;
-    $connection->exec($sql);
+    $connection->executeStatement($sql);
     $sql = <<<'SQL'
 ALTER TABLE `resource_template_property_data`
 CHANGE `data` `data` LONGTEXT NOT NULL COMMENT '(DC2Type:json)';
 SQL;
-    $connection->exec($sql);
+    $connection->executeStatement($sql);
 }
